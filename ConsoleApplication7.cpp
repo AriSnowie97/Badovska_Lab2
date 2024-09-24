a9d57d7a08467f4e454b29af8ec93dda178f3476
Ari Badovska <160342977+AriSnowie97@users.noreply.github.com>
2024-09-24 19:50:05 +0300

    Add files via upload

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+bool is_palindrome(int num) {
+    int orignum = num;
+    int reversnum = 0;
+    while (num > 0) {
+        int digit = num % 10;
+        reversnum = reversnum * 10 + digit;
+        num /= 10;
+    }
+    return orignum == reversnum;
+}
+
+int main() {  
+        while (true) {
+            int num;
+            cout << "Enter number: " << endl;
+            cin >> num;
+            if (is_palindrome(num)) {
+                cout << "Palindrome" << endl;
+            }
+            else {
+                cout << "Isn't Palindrome" << endl;
+            }
+        }
+    return 0;
+}
